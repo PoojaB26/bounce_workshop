e96fce31929382cdc5cf09df0f4dbdbe748d4f6f
Pooja Bhaumik <pooja.b@bounceshare.com>
2019-07-23 22:13:24 +0530

    removed comments

--- lib/ui/screens/list_screen.dart
+++ lib/ui/screens/list_screen.dart
@@ -63,9 +63,6 @@ class _ListScreenState extends State<ListScreen> {
     super.dispose();
   }
 
-
-
-
   final String username = 'John Dow';
 
   navigate(context){
@@ -97,46 +94,3 @@ class _ListScreenState extends State<ListScreen> {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import 'package:bounce_workshop/ui/common/list_item.dart';
-//import 'package:bounce_workshop/ui/screens/details_screen.dart';
-//import 'package:flutter/material.dart';
-//
-//class ListScreen extends StatelessWidget {
-//
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//        appBar: AppBar(),
-//        floatingActionButton: FloatingActionButton(
-//          child: Icon(Icons.navigate_next),
-//          onPressed: () => {},
-//        ),
-//        body: ListView.builder(
-//            itemCount: 10,
-//            itemBuilder: (context, index) => ListItem(index: index,)));
-//  }
-//
-//
-//}
-//
